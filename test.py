59ed500badc8f250ecff7aa91ac78eec2d37a275
Olga Melnikova <shelga.melnikova@gmail.com>
2021-10-07 19:03:05 +0100

    send in progress

--- test.py
+++ test.py
@@ -15,17 +15,11 @@ bot = telebot.TeleBot(token, parse_mode=None)
 
 
 
-
-
-
-
-
-
 @bot.message_handler(commands=['send'])
 def send_welcome(message):
     bot.reply_to(message, "Enter your weight")
 
-
+@bot.message_handler(func=lambda message: True)
 def save_to_db(message):
     # connect to the database
     connect = sqlite3.connect('message.db')
@@ -45,39 +39,16 @@ def save_to_db(message):
     date_message = datetime.datetime.fromtimestamp(int(message.date)).strftime('%Y-%m-%d %H:%M:%S')
     message_user = message.text
     
-
     params = (id_user, date_message, message_user)
+    print((id_user, date_message, message_user))
 
-
-    # check the first character of the entered message
-    # first_character = string_int[0]
-    # print("first_character", first_character)
-
-
-
-    # if first_character != "/":
-    #     if message_user.strip().isdigit():
-            
-
-    #         cursor.execute("INSERT INTO weight_from VALUES (NULL, ?, ?, ?)", params)
-    #         connect.commit()
-
-    #         previous_values = cursor.execute("SELECT * FROM weight_from ORDER BY id DESC LIMIT 1;")
-    #         previous_values = cursor.fetchone()
-
-            # try:
-            #     string_int = int(previous_values)
-            #     print(string_int)
-            # except ValueError:
-            #     print('Please enter an integer')
-
-        #     print("send!!!!",type(int((previous_values[3]))))
-        #     print("User input is Number")
-        #     bot.send_message(message.chat.id, f"Your weight is: {message_user}")
-        #     print("here")
-        # else:
-        #     bot.send_message(message.chat.id, "Yours input is string")
-        #     print("User input is string")
+    if message_user.isdigit():
+        cursor.execute("INSERT INTO weight_from VALUES (NULL, ?, ?, ?)", params)
+        connect.commit()
+        print("Done!")
+    else:
+        bot.send_message(message.chat.id, "Yours input is string")
+        print("User input is string")
 
     
     
