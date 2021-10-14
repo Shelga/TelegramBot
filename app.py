47517ddfbce8a7e903b9aacce6fdd88d85e06634
Olga Melnikova <shelga.melnikova@gmail.com>
2021-10-14 16:40:45 +0100

    dates is done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.dates import datetime as dt
+from matplotlib.dates import DateFormatter
 # from test import previous_values_w
 
 # x_list = [1, 2, 3, 4, 5, 6, 7]
@@ -19,17 +20,33 @@ from matplotlib.dates import datetime as dt
 # t = ['2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13']
 # s = [49.9, 49.4, 49.7, 49.2, 49.0, 49.1, 49.3]
 
-dates= ['2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13']
-t = [dt.datetime.strptime('%Y-%m-%d') for d in dates]
-print(t)
-s = [49.9, 49.4, 49.7, 49.2, 49.0, 49.1, 49.3]
 
-fig, ax = plt.subplots()
-ax.plot(t, s)
 
-ax.set(xlabel='time (d)', ylabel='weight (kg)',
-       title='Weight change chart for 7 days')
-ax.grid()
+# dates= ['2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13', '2021-10-13']
+# t = [dt.datetime.strptime(d, '%Y-%m-%d') for d in dates]
+# print(t)
+# s = [49.9, 49.4, 49.7, 49.2, 49.0, 49.1, 49.3]
 
-fig.savefig("test.png")
+# fig, ax = plt.subplots()
+# ax.plot(t, s)
+
+# ax.set(xlabel='time (d)', ylabel='weight (kg)',
+#        title='Weight change chart for 7 days')
+# ax.grid()
+
+# fig.savefig("test.png")
+# plt.show()
+
+
+formatter = DateFormatter('%d.%m')
+
+f = plt.figure()
+ax = f.add_subplot(111)
+
+raw_dates = ['2021-10-13', '2021-10-14', '2021-10-14', '2021-10-16', '2021-10-17', '2021-10-18', '2021-10-19']
+x = [dt.datetime.strptime(d, '%Y-%m-%d') for d in raw_dates]
+y = [49.9, 49.4, 49.7, 49.2, 49.0, 49.1, 49.3]
+ax.plot(x, y)
+
+ax.xaxis.set_major_formatter(formatter)
 plt.show()
