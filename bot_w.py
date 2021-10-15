f8b36274ea00f2f70b63dd6b0e5b2e97a50cf096
Olga Melnikova <shelga.melnikova@gmail.com>
2021-10-15 16:12:50 +0100

    previous_week is done

--- bot_w.py
+++ bot_w.py
@@ -5,7 +5,13 @@ import datetime
 import os
 from dotenv import load_dotenv
 
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.dates import datetime as dt
+from matplotlib.dates import DateFormatter
 
+## Settings for matplot grid
+fig, ax = plt.subplots()
 
 load_dotenv()
 
@@ -51,7 +57,7 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def save_to_db(message):
-    # connect to the database
+    ## connect to the database
     connect = sqlite3.connect('message.db')
     cursor = connect.cursor()
 
@@ -62,10 +68,9 @@ def save_to_db(message):
     )""")
 
     connect.commit()
-    # cursor.close()
 
 
-    # user message data
+    ## user message data
     id_user = message.chat.id
     date_message = datetime.datetime.fromtimestamp(int(message.date)).strftime('%Y-%m-%d')
     message_user = message.text
@@ -101,6 +106,72 @@ def send_previous_values(message):
     print("previous_values", previous_values[0])
     bot.send_message(message.chat.id, f"Your previous weight is: {previous_values[0]}")
     
+
+
+
+
+@bot.message_handler(commands=['show_previous_week'])
+def send_previous_values(message):
+
+
+    connect = sqlite3.connect('message.db')
+    cursor = connect.cursor()
+
+    previous_values_p = cursor.execute("SELECT message FROM weight_from ORDER BY id DESC LIMIT 7;")
+    previous_values_p = cursor.fetchall()
+
+    previous_values_w = []
+    for i in previous_values_p:
+        previous_values_w.append(float(i[0]))
+    previous_values_w.reverse()
+
+    previous_days_p = cursor.execute("SELECT date FROM weight_from ORDER BY id DESC LIMIT 7;")
+    previous_days_p = cursor.fetchall()
+
+    previous_days_w = []
+    for i in previous_days_p:
+        previous_days_w.append(i[0])
+    previous_days_w.reverse()
+
+    print("previous_days_w", previous_days_w)
+    print("previous_values_w", previous_values_w)
+    connect.commit()
+
+
+    x = previous_days_w
+    ax.clear()
+    
+    plt.subplot(111)
+    ax.set(xlabel='time (days)', ylabel='weight (kg)',
+       title='Weight change chart for 7 days')
+
+    ax.grid(which='major',
+        color = 'k',
+        linewidth = 1,
+        linestyle = ':')
+    ax.minorticks_on()
+  
+    y = previous_values_w
+  
+    ax.plot(x, y, color = 'b', linewidth = 3)
+
+
+    ax.tick_params(axis = 'both',   
+        which = 'major', 
+        pad = 5,
+        labelsize = 7,
+        labelbottom = True, 
+        labelleft = True,
+        labelrotation = 45) 
+
+    fig.savefig("test.png")
+
+    file = open('test.png', 'rb')
+
+    bot.send_photo(message.chat.id, file)
+
+
+
 
 
 print('STARTED...')
--- test.py
+++ test.py
@@ -7,62 +7,84 @@ from dotenv import load_dotenv
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.dates import datetime as dt
+from matplotlib.dates import DateFormatter
+
+## Settings for matplot grid
+fig, ax = plt.subplots()
 
 
 
+
+## Loading ENV parameters
 load_dotenv()
-
 token = os.getenv('TOKEN')
 
+## Starting a Telegram bot
 bot = telebot.TeleBot(token, parse_mode=None)
 
-@bot.message_handler(commands=['show_previous_week'])
-def send_previous_values(message):
+# @bot.message_handler(commands=['show_previous_week'])
+# def send_previous_values(message):
 
 
-    connect = sqlite3.connect('message.db')
-    cursor = connect.cursor()
+#     connect = sqlite3.connect('message.db')
+#     cursor = connect.cursor()
 
-    previous_values_p = cursor.execute("SELECT message FROM weight_from ORDER BY id DESC LIMIT 7;")
-    previous_values_p = cursor.fetchall()
+#     previous_values_p = cursor.execute("SELECT message FROM weight_from ORDER BY id DESC LIMIT 7;")
+#     previous_values_p = cursor.fetchall()
 
-    previous_values_w = []
-    for i in previous_values_p:
-        previous_values_w.append(float(i[0]))
-    previous_values_w.reverse()
+#     previous_values_w = []
+#     for i in previous_values_p:
+#         previous_values_w.append(float(i[0]))
+#     previous_values_w.reverse()
 
-    previous_days_p = cursor.execute("SELECT date FROM weight_from ORDER BY id DESC LIMIT 7;")
-    previous_days_p = cursor.fetchall()
+#     previous_days_p = cursor.execute("SELECT date FROM weight_from ORDER BY id DESC LIMIT 7;")
+#     previous_days_p = cursor.fetchall()
 
-    previous_days_w = []
-    for i in previous_days_p:
-        previous_days_w.append(i[0])
-    previous_days_w.reverse()
+#     previous_days_w = []
+#     for i in previous_days_p:
+#         previous_days_w.append(i[0])
+#     previous_days_w.reverse()
 
-    print("previous_days_w", previous_days_w)
-    print("previous_values_w", previous_values_w)
-    connect.commit()
-
-
-    # t = previous_days_w
-    # s = previous_values_w
-
-    # fig, ax = plt.subplots()
-    # ax.plot(t, s)
-
-    # ax.set(xlabel='time (d)', ylabel='weight (kg)',
-    #    title='Weight change chart for 7 days')
-    # ax.grid()
-
-    # fig.savefig("test.png")
-    # # plt.show()
-
-    # file = open('test.png', 'rb')
+#     print("previous_days_w", previous_days_w)
+#     print("previous_values_w", previous_values_w)
+#     connect.commit()
 
 
-    # bot.send_photo(message.chat.id, file)
-    # bot.send_photo(message.chat.id, "FILEID")
-#     # bot.send_message(message.chat.id, f"Your previous weight is: {previous_values_w}")
+#     x = previous_days_w
+#     ax.clear()
+    
+#     plt.subplot(111)
+#     ax.set(xlabel='time (days)', ylabel='weight (kg)',
+#        title='Weight change chart for 7 days')
+
+#     ax.grid(which='major',
+#         color = 'k',
+#         linewidth = 1,
+#         linestyle = ':')
+#     ax.minorticks_on()
+  
+#     y = previous_values_w
+  
+#     ax.plot(x, y, color = 'b', linewidth = 3)
+
+
+#     ax.tick_params(axis = 'both',   
+#         which = 'major', 
+#         pad = 5,
+#         labelsize = 7,
+#         labelbottom = True, 
+#         labelleft = True,
+#         labelrotation = 45) 
+
+#     fig.savefig("test.png")
+
+#     file = open('test.png', 'rb')
+
+#     bot.send_photo(message.chat.id, file)
+
+
+#     
 
 
 
